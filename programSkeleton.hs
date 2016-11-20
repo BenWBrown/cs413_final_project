ebacb1ce2d939cb0dfe34e951a40cde1b56f49bd
Ellen Sartorelli <esartorelli@middlebury.edu>
2016-11-20 17:27:05 -0500

    more changes to skeleton

--- programSkeleton.hs
+++ programSkeleton.hs
@@ -11,6 +11,27 @@ not' :: Bit -> Bit
 and', or', xor' :: Bit -> Bit -> Bit
 
 
+-- Circuit
+data Circuit = Circuit {
+      runCircuit :: [String] -> [Maybe String]
+}
+
+-- Input
+data Input = Input {
+      bitWidth :: String, --read in from parser?
+      connection :: String
+      -- parent field??? ex: if it's an input to a logic function
+}
+
+-- Constant
+data Constant = Constant {
+       bitWidth :: String,
+       value :: [Bit], --value may or may not start in base 2. value :: toBin v?
+       connection :: String
+}
+
+
+
 
 --Functions:
 
