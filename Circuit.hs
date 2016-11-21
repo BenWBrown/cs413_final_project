a2081c7709da6c5e16b93e068e737116dd89ae72
Ben Brown <Ben@Bens-MacBook-Pro-6.local>
2016-11-21 10:45:53 -0800

    add LogicElement data type to Circuit.hs

--- Circuit.hs
+++ Circuit.hs
@@ -21,4 +21,13 @@ data ConnectedElement = Input {
        bitWidth :: String,
        value :: [Bit], --value may or may not start in base 2. value :: toBin v?
        connection :: String
-} --TODO: add logic part??
+}
+
+--logic element. a is the type of the function that logic element runs
+data LogicElement a = LogicElement a [ConnectedElement] [ConnectedElement]
+
+inputs :: LogicElement a -> [ConnectedElement]
+inputs (LogicElement _ inputs _) = inputs
+
+outputs :: LogicElement a -> [ConnectedElement]
+outputs (LogicElement _ _ outputs) = outputs
